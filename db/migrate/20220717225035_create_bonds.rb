90b4c427e773d89ff46afa0cc0f1ab346a160183
amrosh <amrosh_world@gmail.com>
2022-07-20 14:16:27 +0200

    Added my project

---
+++ db/migrate/20220717225035_create_bonds.rb
@@ -0,0 +1,12 @@
+class CreateBonds < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bonds do |t|
+      t.bigint :user_id , null: false
+      t.bigint :friend_id , null: false
+      t.string :state , null: false
+
+      t.timestamps
+    end
+    add_index :bonds, [:user_id,:friend_id] ,unique: true
+  end
+end
